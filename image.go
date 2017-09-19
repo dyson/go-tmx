4fc6191a49db31c6155e72e57f91b645a478493b
Dyson Simmons <dysonsimmons@gmail.com>
2017-09-19 18:06:20 +0100

    Updated image and layer objects

--- image.go
+++ image.go
@@ -1,7 +1,3 @@
-// Copyright 2012 Lightpoke. All rights reserved.
-// This source code is subject to the terms and
-// conditions defined in the "License.txt" file.
-
 package tmx
 
 import (
@@ -11,23 +7,14 @@ import (
 
 // Image represents the source and properties of a image
 type Image struct {
-	// Format of the embedded image data (if any).
 	Format string
-
-	// The file path at which the image may be found
 	Source string
-
-	// The color in the image representing transparency (if any).
-	//
-	// The alpha (A) component of the color will always be 255.
-	Trans color.RGBA
-
-	// The width and height of the image (useful mostly only for correction
-	// when the image's size changes from that known to the TMX file).
-	Width, Height int
+	Trans  color.RGBA
+	Width  int
+	Height int
 }
 
-// String returns a string representation of this image.
+// String returns a string representation of this image
 func (i *Image) String() string {
 	return fmt.Sprintf("Image(Source=%q, Size=%dx%dpx)", i.Source, i.Width, i.Height)
 }
--- layer.go
+++ layer.go
@@ -1,29 +1,17 @@
-// Copyright 2012 Lightpoke. All rights reserved.
-// This source code is subject to the terms and
-// conditions defined in the "License.txt" file.
-
 package tmx
 
 import (
 	"fmt"
 )
 
-// Coord represents a single 2D coordinate pair (x, y)
-type Coord struct {
-	X, Y int
-}
-
 // Layer represents a single map layer and all of it's tiles
 type Layer struct {
-	// The name of the layer.
-	Name string
-
-	// Value between 0 and 1 representing the opacity of the layer.
-	Opacity float64
-
-	// Boolean value representing whether or not the layer is visible.
-	Visible bool
-
+	Name       string
+	Opacity    float64
+	Visible    bool
+	OffsetX    int
+	OffsetY    int
+	Properties map[string]string
 	// A map of 2D coordinates in this layer to so called "global tile IDs"
 	// (gids).
 	//
@@ -35,10 +23,15 @@ type Layer struct {
 	// tile is you need to find the tileset with the highest Firstgid that is
 	// still lower or equal than the gid. The tilesets are always stored with
 	// increasing firstgids.
-	Tiles map[Coord]uint32
+	Tiles map[Coord]uint32 // from data
 }
 
-// String returns a string representation of this layer.
+// String returns a string representation of this layer
 func (l *Layer) String() string {
 	return fmt.Sprintf("Layer(Name=%q, Opacity=%1.f, Visible=%v)", l.Name, l.Opacity, l.Visible)
+}
+
+// Coord represents a single 2D coordinate pair (x, y)
+type Coord struct {
+	X, Y int
 }
