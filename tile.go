ca239b6fc7e0181b9a4cf501a69955a8f2b94004
Dyson Simmons <dysonsimmons@gmail.com>
2017-09-19 19:29:48 +0100

    Remove xmlTile object from tile object

--- tile.go
+++ tile.go
@@ -23,47 +23,6 @@ const (
 	flippedDiagonallyFlag   uint32 = 0x20000000
 )
 
-type xmlTile struct {
-	ID          int           `xml:"id,attr"`
-	Terrain     []byte        `xml:"terrain,attr"`
-	Probability float64       `xml:"probability,attr"`
-	Properties  xmlProperties `xml:"properties"`
-	Image       xmlImage      `xml:"image"`
-}
-
-func (x xmlTile) terrainArray() (indices [4]int) {
-	var (
-		buf = bytes.NewBuffer(x.Terrain)
-		k   = 0
-	)
-	indices = [4]int{-1, -1, -1, -1}
-	terrain, err := csv.NewReader(buf).ReadAll()
-	if err != nil {
-		return
-	}
-	for _, i := range terrain {
-		for _, j := range i {
-			index, _ := strconv.Atoi(j)
-			indices[k] = index
-			k++
-			if k >= 4 {
-				return
-			}
-		}
-	}
-	return
-}
-
-func (x xmlTile) toTile() *Tile {
-	return &Tile{
-		ID:          x.ID,
-		Terrain:     x.terrainArray(),
-		Probability: x.Probability,
-		Properties:  x.Properties.toMap(),
-		Image:       x.Image.toImage(),
-	}
-}
-
 // Tile represents a single tile definition and it's properties
 type Tile struct {
 	// The ID of the tile
